20f34a5c25822bcd954513a757513d22d466b31d
push0012 <pushpamal0012@gmail.com>
2019-08-23 21:18:47 +0530

    commented some funtions and leave only show json respond

--- app/Http/Controllers/TimeTableController.php
+++ app/Http/Controllers/TimeTableController.php
@@ -23,32 +23,32 @@ class TimeTableController extends Controller
         
         $coz_subs = $course_subject->where($condition)->get()->toArray();
  
-        $levelWideCourses = null;
+        /*$levelWideCourses = null;
         $newCondition = $condition;
-        //$newCondition['course_code'] = 0;
+        $newCondition['course_code'] = 0;
 
         if ($table->alreadyHas($newCondition)) {
 
            $levelWideCourses = $table->where($newCondition)->first()->schedule;
            
-        }
+        }*/
 
         try{
             $timeTable =  new TimeTableGenerator($coz_subs);
-            $timeTable->levelWideCourse(json_decode($levelWideCourses, true));
+            //$timeTable->levelWideCourse(json_decode($levelWideCourses, true));
             $timeTable = $timeTable->generate($condition['course_code']);
 
-            if ($table->alreadyHas($condition)) {
+            /*if ($table->alreadyHas($condition)) {
                 $table->where($condition)->update(['schedule' => json_encode($timeTable) ]);
-            } else {
-                $tables = $table->create([
+            } else {*/
+                /*$tables = $table->create([
                     'course_code'          => $condition['course_code'] ?? 0,
                     //'level'         => $condition['level'],
                     'semester'      => $condition['semester'],
                     'ac_year'       => $condition['ac_year'],
                     'schedule'      => json_encode($timeTable)
-                ]);
-            }
+                ]);*/
+            //}
             return response()->json($timeTable, 201);
         } catch (\Exception $e)
         {
