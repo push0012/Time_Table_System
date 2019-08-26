c0f0889c425aef520cda4a201149879f3568d374
push0012 <pushpamal0012@gmail.com>
2019-08-26 15:13:24 +0530

    add css and js file urls and include navbar and footer files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,17 +11,73 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Modernizr Js -->
+    <script src="{{ asset('js/modernizr-2.8.3.min.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
 
+    <!-- Normalize CSS -->
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <!--Bootstrap CSS -->
+ 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="{{ asset('css/animate.min.css') }}">
+    <!-- Font-awesome CSS-->
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <!-- Owl Caousel CSS -->
+    <link rel="stylesheet" href="{{ asset('vendor/OwlCarousel/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/OwlCarousel/owl.theme.default.min.css') }}">
+    <!-- Main Menu CSS -->
+    <link rel="stylesheet" href="{{ asset('css/meanmenu.min.css') }}">
+    <!-- Select2 CSS -->
+    <link rel="stylesheet" href="{{ asset('css/select2.min.css') }}">
+    <!-- Magnific CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/magnific-popup.css') }}">
+    <!-- Switch Style CSS -->
+    <link rel="stylesheet" href="{{ asset('css/hover-min.css') }}">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <script src="{{ asset('js/jquery-2.2.4.min.js') }} " type="text/javascript"></script>
+    <!-- Plugins js -->
+    <script src="{{ asset('js/plugins.js') }} " type="text/javascript"></script>
+    <!-- Bootstrap js -->
+    <script src="{{ asset('js/bootstrap.min.js') }} " type="text/javascript"></script>
+    <!-- WOW JS -->
+    <script src="{{ asset('js/wow.min.js') }}"></script>
+    <!-- Owl Cauosel JS -->
+    <script src="{{ asset('vendor/OwlCarousel/owl.carousel.min.js') }} " type="text/javascript"></script>
+    <!-- Meanmenu Js -->
+    <script src="{{ asset('js/jquery.meanmenu.min.js') }} " type="text/javascript"></script>
+    <!-- Srollup js -->
+    <script src="{{ asset('js/jquery.scrollUp.min.js') }} " type="text/javascript"></script>
+    <!-- jquery.counterup js -->
+    <script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('js/waypoints.min.js') }}"></script>
+    <!-- Select2 Js -->
+    <script src="{{ asset('js/select2.min.js') }}" type="text/javascript"></script>
+    <!-- Isotope js -->
+    <script src="{{ asset('js/isotope.pkgd.min.js') }} " type="text/javascript"></script>
+    <!-- Magnific Popup -->
+    <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+    <!-- Custom Js -->
+    <script src="js/main.js') }} " type="text/javascript"></script>
 </head>
 <body>
+
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+       @include('navbar')
+       
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="position: absolute;">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -75,6 +131,7 @@
         <main class="py-4">
             @yield('content')
         </main>
+        @include('footer')
     </div>
 </body>
 </html>
