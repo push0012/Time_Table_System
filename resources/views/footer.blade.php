48845f65cc512bc3bb2644707c5769f1882e9245
push0012 <pushpamal0012@gmail.com>
2019-08-26 15:14:04 +0530

    footer template file

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,89 @@
+<footer>
+            <div class="footer-area-top s-space-equal">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-mb-12">
+                            <div class="footer-box">
+                                <h3 class="title-medium-light title-bar-left size-lg">About us</h3>
+                                <ul class="useful-link">
+                                    <li>
+                                        <a href="about.html">About us</a>
+                                    </li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-mb-12">
+                            <div class="footer-box">
+                                <h3 class="title-medium-light title-bar-left size-lg">Conditions and policy</h3>
+                                <ul class="useful-link">
+								
+								
+								    <li>
+                                        <a href="#">Terms &amp; Conditions</a>
+                                    </li>
+								    <li>&nbsp;</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-mb-12">
+                            <div class="footer-box">
+                                <h3 class="title-medium-light title-bar-left size-lg">Help &amp; Support</h3>
+                                <ul class="useful-link">
+                                    <li>
+                                        <a href="#">Groupnumber6@gmail.com</a>
+                                    </li>
+                                    
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-mb-12">
+                            <div class="footer-box">
+                                <h3 class="title-medium-light title-bar-left size-lg">Follow Us On</h3>
+                                <ul class="social-link">
+                                   
+                                    <li class="tw-classipost">
+                                        <a href="#">
+                                             <a class="btn btn-social-icon btn-twitter">
+											<span class="fa fa-twitter"></span>
+											</a>
+                                        </a>
+                                    </li>
+                                    <li class="yo-classipost">
+                                        <a href="#">
+                                             <a class="btn btn-social-icon btn-twitter">
+											<span class="fa fa-facebook"></span>
+											</a>
+											
+                                        </a>
+                                    </li>
+                                    <li class="pi-classipost">
+                                        <a class="btn btn-social-icon btn-twitter">
+                                            <span class="fa fa-linkedin"></span>
+                                        </a>
+                                    </li>
+                                    <li class="li-classipost">
+                                        <a class="btn btn-social-icon btn-twitter">
+                                            <span class="fa fa-github"></span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-area-bottom">
+                <div class="container">
+                    <div class="row">
+					<center>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 col-mb-12 text-center-mb">
+                            <p>Copyright © group number six</p>
+                        </div>
+						</center>
+                        
+                    </div>
+                </div>
+            </div>
+        </footer>
