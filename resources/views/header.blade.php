e3d7a6bae00f9d8ee3d857f006f064f22c540958
push0012 <pushpamal0012@gmail.com>
2019-08-26 15:14:40 +0530

    header template file

---
+++ resources/views/header.blade.php
@@ -0,0 +1,42 @@
+
+<section class="search-layout1 bg-body full-width-border-bottom fixed-menu-mt">
+            <div class="container">
+                <form id="cp-search-form">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div class="form-group search-input-area input-icon-location">
+                                <select id="location" class="select2">
+                                    <option class="first" value="0">Location</option>
+                                    <option value="1">New Building</option>
+                                    <option value="2">CEC</option>
+                                    <option value="3">New Media Complex</option>
+                                    <option value="3">Old Building</option>
+                                    
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                            <div class="form-group search-input-area input-icon-category">
+                                <select id="categories" class="select2">
+                                    <option class="first" value="0">Select Categories</option>
+                                    <option value="1">Lab</option>
+                                    <option value="2">Class Room</option>
+                                    <option value="3">Auditoriam</option>
+                                    
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-3 col-sm-8 col-xs-8 col-mb-12">
+                            <div class="form-group search-input-area input-icon-keywords">
+                                <input placeholder="Enter Keywords here ..." value="" name="key-word" type="text">
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-3 col-sm-4 col-xs-4 col-mb-12 text-right text-left-mb">
+                            <a href="#" class="cp-search-btn">
+                                <i class="fa fa-search" aria-hidden="true"></i>Search
+                            </a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
