4e8f8957c38c6c98b395ff2f5768a7e3fee303a2
push0012 <pushpamal0012@gmail.com>
2019-08-27 15:39:15 +0530

    change home page image

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,9 +9,9 @@
         </div>
         <div class="row">
             <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"></div>
-            <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><img src="{{ asset('image/1.jpg') }}"/></div>
+            <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7"><img style="height:175px;"  src="{{ asset('image/uni.png') }}"/></div>
         </div>        
-    </div>
+    </div> 
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
