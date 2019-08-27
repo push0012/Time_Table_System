9bc99fb4874046003fe496482fc49185e1f72232
push0012 <pushpamal0012@gmail.com>
2019-08-27 11:29:46 +0530

    change js file type, move js links to bottom, remove all navbar things

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }} " type="text/javascript" defer></script>
     <!-- Modernizr Js -->
-    <script src="{{ asset('js/modernizr-2.8.3.min.js') }}"></script>
+    <script src="{{ asset('js/modernizr-2.8.3.min.js') }}" type="text/javascript"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -47,13 +47,27 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    
+</head>
+<body>
+    <div id="app">
+    <div id="wrapper">   
+       @include('navbar')
+       
+        <main class="py-4">
+            @yield('content')
+        </main>
+        @include('footer')
+    </div>
+    </div>
+
     <script src="{{ asset('js/jquery-2.2.4.min.js') }} " type="text/javascript"></script>
     <!-- Plugins js -->
     <script src="{{ asset('js/plugins.js') }} " type="text/javascript"></script>
     <!-- Bootstrap js -->
     <script src="{{ asset('js/bootstrap.min.js') }} " type="text/javascript"></script>
     <!-- WOW JS -->
-    <script src="{{ asset('js/wow.min.js') }}"></script>
+    <script src="{{ asset('js/wow.min.js') }}" type="text/javascript"></script>
     <!-- Owl Cauosel JS -->
     <script src="{{ asset('vendor/OwlCarousel/owl.carousel.min.js') }} " type="text/javascript"></script>
     <!-- Meanmenu Js -->
@@ -61,77 +75,15 @@
     <!-- Srollup js -->
     <script src="{{ asset('js/jquery.scrollUp.min.js') }} " type="text/javascript"></script>
     <!-- jquery.counterup js -->
-    <script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
-    <script src="{{ asset('js/waypoints.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.counterup.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/waypoints.min.js') }}" type="text/javascript"></script>
     <!-- Select2 Js -->
     <script src="{{ asset('js/select2.min.js') }}" type="text/javascript"></script>
     <!-- Isotope js -->
     <script src="{{ asset('js/isotope.pkgd.min.js') }} " type="text/javascript"></script>
     <!-- Magnific Popup -->
-    <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.magnific-popup.min.js') }}" type="text/javascript"></script>
     <!-- Custom Js -->
-    <script src="js/main.js') }} " type="text/javascript"></script>
-</head>
-<body>
-
-    <div id="app">
-       @include('navbar')
-       
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="position: absolute;">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-        @include('footer')
-    </div>
+    <script src="{{ asset('js/main.js') }} " type="text/javascript"></script>
 </body>
 </html>
