0c4c515d76a0ac3ff6e3a1f0e62cb42cfdccecfc
push0012 <pushpamal0012@gmail.com>
2019-09-03 11:23:48 +0530

    col-8 to col-12, top options (course, yer, sem),th style width,remove bottom search part

--- resources/views/master/subject/subject.blade.php
+++ resources/views/master/subject/subject.blade.php
@@ -3,72 +3,82 @@
 @section('content')
 
 <section class="s-space-bottom-full bg-accent-shadow-body">
-            <div class="container">
-         
-            </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-9 col-md-8 col-sm-8 col-xs-12">
-                        <div class="gradient-wrapper item-mb">
-                            <div class="gradient-title">
-                                <h2>Add Subjects</h2>
-                            </div>
-                            <div class="input-layout1 gradient-padding">
-                               
-                                <div class="section-title-left-dark child-size-xl title-bar item-mb">
-                                    <h3>Add Subjects Here</h3>
-                                </div> 
-                                    <form id="subjectfrm" >
-                                        <input class="btn btn-outline-primary btn-sm" type="button" id="addRow" value="Add Subject">
-                                        <input class="btn btn-outline-danger btn-sm" type="button" id="remove" value="Remove Row">
-                                    <table id="example" class="display" style="width:100%">
-                                        <thead>
-                                            <tr>
-                                                <th>Subject ID</th>
-                                                <th>Subject Name</th>
-                                                <th>Method</th>
-                                                <th>Needs</th>
-                                                <th>Credits</th>
-                                            </tr>
-                                        </thead>
-                                    </table>
-                                    <br>
-                            <center>									
-							    <button class="default-big-btn" id="reddit" type="button" value="Submit">Submit</button>
-                                <button class="default-big-btn form-cancel" type="button" value="">Cancel</button>		
-                            </center>
-   
-                                    
-                            </div>
-                        </div>
-         
-                  
+    <div class="container">
+       
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="gradient-wrapper item-mb">
+                    <div class="gradient-title">
+                        <h2>Add Subjects</h2>
                     </div>
-                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <div class="sidebar-item-box">
-                            <div class="gradient-wrapper">
-                                <div class="gradient-title">
-                                    <h3>Search Subject Details</h3>
+                    <div class="input-layout1 gradient-padding">
+                     
+                        <div class="section-title-left-dark child-size-xl title-bar item-mb">
+                            <h3>Add Subjects Detail Plan Here</h3>
+                        </div> 
+                        <form id="subjectfrm" >
+                            <div class="form-group row">
+                                <label for="course_code" class="col-md-2 col-form-label text-md-left">{{ __('Course Name') }}</label>
+                                <div class="col-md-6">
+                                    <select class="form-control" name="course_code" id="course_code">
+                                        <option value="0" disabled selected>Select Course ...</option>
+                                        @foreach($courses as $course)
+                                        <option value="{{$course->course_code}}">{{$course->course_name}}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
-                                <ul class="sidebar-seller-information">
-									   <li><center>
-                                       Enter Subject ID/Name</br>
-									   <input type="text" id="search_class_room"/>
-									   </br>
-									   <a href="#" class="btn btn-default">
-									   <i class="fa fa-search" aria-hidden="true"></i>Search
-									   </a></center>
-									   
-                                   
-                                
-                                 
-                                </ul>
                             </div>
-                        </div>
-                    
-                       
-                    </div>
-                </div>
-            </div>
-        </section>
-@endsection
+
+                            <div class="form-group row">
+                                <label for="ac_year" class="col-md-2 col-form-label text-md-left">{{ __('Academic Year') }}</label>
+                                <div class="col-md-6">
+                                    <select class="form-control" name="ac_year" id="ac_year">
+                                        <option value="0" disabled selected>Select Year ...</option>
+                                        @for ($i = 2016; $i <= 2020; $i++)
+                                        <option value="{{ $i }}">{{ $i }}</option>
+                                        @endfor
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="semester" class="col-md-2 col-form-label text-md-left">{{ __('Semester') }}</label>
+                                <div class="col-md-6">
+                                    <select class="form-control" name="semester" id="semester">
+                                        <option value="0" disabled selected>Select Semester ...</option>
+                                        @for ($i = 1; $i <= 6; $i++)
+                                        <option value="{{ $i }}">Semester {{ $i }}</option>
+                                        @endfor
+                                    </select>
+                                </div>
+                            </div>
+
+                            <input class="btn btn-outline-primary btn-sm" type="button" id="addRow" value="Add Subject">
+                            <input class="btn btn-outline-danger btn-sm" type="button" id="remove" value="Remove Row">
+                                                        
+                            <table id="example" class="display" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th style="max-width:15px !important;">Subject ID</th>
+                                        <th style="min-width: 150px !important;">Subject Name</th>
+                                        <th >Method</th>
+                                        <th >Needs</th>
+                                        <th style="min-width: 105px !important;">Lecturer</th>
+                                        <th >Credits</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                            <br>
+                            <center>									
+                             <button class="default-big-btn" id="reddit" type="button" value="Submit">Submit</button>
+                             <button class="default-big-btn form-cancel" type="button" value="">Cancel</button>		
+                         </center>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
+ @endsection
