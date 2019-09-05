a98f5eaeee005cc532803ea0c4165a470672f413
push0012 <pushpamal0012@gmail.com>
2019-09-05 23:02:45 +0530

    create dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,55 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header"><h1>Dashboard</h1></div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="container justify-content-center">
+                        <div class="row justify-content-center">
+                            <div class="col-md-6 dash">
+                                <a href="{{ url('classroom') }}" class="linkdash">
+                                    <i class="fas fa-chalkboard fa-5x"></i><br>
+                                    <span class="badge badge-secondary names">Classroom</span>
+                                </a>
+                            </div>
 
-                    You are logged in!
+                            <div class="col-md-6 dash">
+                                <a href="{{ url('course') }}" class="linkdash">
+                                    <i class="fas fa-award fa-5x"></i><br>
+                                    <span class="badge badge-secondary names">Course</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-md-6 dash">
+                                <a href="{{ url('subject') }}" class="linkdash">
+                                    <i class="fas fa-laptop-code fa-5x"></i><br>
+                                    <span class="badge badge-secondary names">Subject</span>
+                                </a>
+                            </div>
+                            <div class="col-md-6 dash">
+                                <a href="{{ url('lecturer') }}" class="linkdash">
+                                    <i class="fas fa-chalkboard-teacher fa-5x"></i><br>
+                                    <span class="badge badge-secondary names">Lecturer</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-md-6 dash">
+                                <a href="{{ url('course_subject') }}" class="linkdash">
+                                    <i class="fas fa-shapes fa-5x"></i><br>
+                                    <span class="badge badge-secondary names">Semester Plan</span>
+                                </a>
+                            </div>
+
+                            <div class="col-md-6 dash">
+                                <a href="{{ url('course') }}" class="linkdash">
+                                    <i class="fas fa-calendar-alt fa-5x"></i><br>
+                                    <span class="badge badge-secondary names">Timetable</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
