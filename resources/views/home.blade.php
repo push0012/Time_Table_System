fdf25b6edb49fcc0c17229404d43203db8587fec
push0012 <pushpamal0012@gmail.com>
2019-09-06 12:02:56 +0530

    add timetable modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -110,10 +110,73 @@
                             </div>
 
                             <div class="col-md-6 dash">
-                                <a href="{{ url('course') }}" class="linkdash">
+                                <a href="#" data-toggle="modal" data-target="#myModal2" class="linkdash">
                                     <i class="fas fa-calendar-alt fa-5x"></i><br>
                                     <span class="badge badge-secondary names">Timetable</span>
                                 </a>
+                            </div>
+                            <div class="modal fade" id="myModal2">
+                                <div class="modal-dialog modal-lg" style="width:50%;">
+                                <div class="modal-content">
+                                
+                                    <!-- Modal Header -->
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">TimeTable Generating and Viewing</h4>
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    </div>
+                                    
+                                    <!-- Modal body -->
+                                    <div class="modal-body">
+                                        
+                                        <form method="GET" action="{{ url('viewtimetable') }}">
+                                            <div class="form-group row">
+                                                <label for="course_code" class="col-md-4 col-form-label text-md-left">{{ __('Course') }}</label>
+                                                <div class="col-md-8">
+                                                    <select class="form-control" name="course_name" id="course_name">
+                                                        <option value="0" disabled selected>Select Course ...</option>
+                                                        @foreach($courses as $course)
+                                                        <option value="{{ $course->course_name}}">{{ $course->course_name}}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="semester" class="col-md-4 col-form-label text-md-left">{{ __('Semester') }}</label>
+                                                <div class="col-md-8">
+                                                    <select class="form-control" name="semester" id="semester">
+                                                        <option value="0" disabled selected>Select Semester ...</option>
+                                                        <option value="1">Semester 01</option>
+                                                        <option value="2">Semester 02</option>
+                                                        <option value="3">Semester 03</option>
+                                                        <option value="4">Semester 04</option>
+                                                        <option value="5">Semester 05</option>
+                                                        <option value="6">Semester 06</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row">
+                                                <label for="ac_year" class="col-md-4 col-form-label text-md-left">{{ __('Academic Year') }}</label>
+                                                <div class="col-md-8">
+                                                    <select class="form-control" name="ac_year" id="ac_year">
+                                                        <option value="0" disabled selected>Select Year ...</option>
+                                                        @for ($i = 2016; $i <= 2020; $i++)
+                                                        <option value="{{ $i }}">{{ $i }}</option>
+                                                        @endfor
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <center>									
+                                                <button class="default-big-btn form-cancel" type="submit" >View</button>
+                                            </center>
+                                        </form>
+                                    </div>
+                                    
+                                    <!-- Modal footer -->
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-danger" >Generate Time Table</button>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
                         </div>
                     </div>
