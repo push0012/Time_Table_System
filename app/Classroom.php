737d5c450596348539b96f679d40b512918eb461
push0012 <pushpamal0012@gmail.com>
2019-09-07 22:11:25 +0530

    change type fillable to method

--- app/Classroom.php
+++ app/Classroom.php
@@ -11,7 +11,7 @@ class Classroom extends Model
     protected $primaryKey = 'classroom_id';
 
     protected $fillable = [
-        'classroom_name','location','student_capacity','type',
+        'classroom_name','location','student_capacity','method',
         'deleted','user_data'
     ];
 }
